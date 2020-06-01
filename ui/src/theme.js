0b078d5387a970da767cfb76163aa40d52881d7d
abu <admin@techjail.net>
2020-06-01 13:43:02 +0545

    Changed to listo color and icon

--- ui/src/theme.js
+++ ui/src/theme.js
@@ -1,10 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
 
 
 const theme = createMuiTheme({
     palette: {
-      primary: blue,
+      primary: red,
     },
 });
   
